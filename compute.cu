e87f2d32ff75ecb27fa79e001a8b212ac5c1dd86
Josh <joshuatylermartinez@gmail.com>
2023-12-11 14:50:40 -0500

    added check for i, j size

--- compute.cu
+++ compute.cu
@@ -9,17 +9,19 @@ __global__ void accelComputeKernal(vector3** dev_accels, double * dev_mass, vect
 	int j = blockIdx.y * blockDim.y * threadIdx.y;
 	int k = threadIdx.z;
 
-	dev_accels[i]=&dev_values[i*NUMENTITIES];
+	if (i < NUMENTITIES && j < NUMENTITIES) {
+		dev_accels[i]=&dev_values[i*NUMENTITIES];
 
-	if (i==j) {
-		FILL_VECTOR(dev_accels[i][j],0,0,0);
-	}else{
-		vector3 distance;
-		distance[k]= dev_hPos[i][k] - dev_hPos[j][k];
-		double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
-		double magnitude=sqrt(magnitude_sq);
-		double accelmag=-1*GRAV_CONSTANT*dev_mass[j]/magnitude_sq;
-		FILL_VECTOR(dev_accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
+		if (i==j) {
+			FILL_VECTOR(dev_accels[i][j],0,0,0);
+		}else{
+			vector3 distance;
+			distance[k]= dev_hPos[i][k] - dev_hPos[j][k];
+			double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
+			double magnitude=sqrt(magnitude_sq);
+			double accelmag=-1*GRAV_CONSTANT*dev_mass[j]/magnitude_sq;
+			FILL_VECTOR(dev_accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
+		}
 	}
 }
 //compute: Updates the positions and locations of the objects in the system based on gravity.
@@ -30,7 +32,6 @@ void compute(){
 	//make an acceleration matrix which is NUMENTITIES squared in size;
 	int i,j,k;
 	vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
-	vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
 	vector3** dev_accels;
 	double * dev_mass;
 	vector3* dev_hPos;
@@ -49,6 +50,7 @@ void compute(){
 	
 	accelComputeKernal<<<numBlocks, blockSize>>>(dev_accels, dev_mass, dev_hPos, dev_values);
 
+	vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
 	cudaMemcpy(accels, dev_accels, sizeof(vector3*)*NUMENTITIES, cudaMemcpyDeviceToHost);
 	
 	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
