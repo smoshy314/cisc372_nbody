70baff94d960084fb65567d56f5d06aadc9981da
Josh <joshuatylermartinez@gmail.com>
2023-12-11 18:16:25 -0500

    sum rows

--- compute.cu
+++ compute.cu
@@ -33,9 +33,20 @@ __global__ void contructAccels(vector3** dev_accels, vector3* dev_values){
 	}
 }
 
-// __global__ void sumRows(vector3** dev_accels, vector3* dev_hPos, vector3* d_hVel){
+__global__ void sumRows(vector3** dev_accels, vector3* dev_hPos, vector3* d_hVel){
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	int j = blockIdx.y * blockDim.y + threadIdx.y;
+	int k = threadIdx.z;
+
+	vector3 accel_sum={0,0,0};
+	accel_sum[k]+=dev_accels[i][j][k];
+		
+//compute the new velocity based on the acceleration and time interval
+//compute the new position based on the velocity and time interval
+	dev_hVel[i][k]+=accel_sum[k]*INTERVAL;
+	dev_hPos[i][k]+=dev_hVel[i][k]*INTERVAL;
 	
-// }
+}
 //compute: Updates the positions and locations of the objects in the system based on gravity.
 //Parameters: None
 //Returns: None
@@ -54,10 +65,13 @@ void compute(){
 
 	double * dev_mass;
 	vector3* dev_hPos;
+	vector3* dev_hVel;
 	cudaMalloc(&dev_mass, sizeof(double) * NUMENTITIES );
 	cudaMemcpy(dev_mass, mass, sizeof(double) * NUMENTITIES,cudaMemcpyHostToDevice);
 	cudaMalloc(&dev_hPos, sizeof(vector3) * NUMENTITIES );
 	cudaMemcpy(dev_hPos, hPos,sizeof(vector3) * NUMENTITIES,cudaMemcpyHostToDevice);
+	cudaMalloc(&dev_hVel, sizeof(vector3) * NUMENTITIES );
+	cudaMemcpy(dev_hVel, hvel,sizeof(vector3) * NUMENTITIES,cudaMemcpyHostToDevice);
 	
 	dim3 blockSize(18,18,3);
 	dim3 numBlocks((NUMENTITIES+323)/324);
@@ -66,25 +80,16 @@ void compute(){
 	accelComputeKernal<<<numBlocks, blockSize>>>(dev_accels, dev_mass, dev_hPos);
 	cudaError_t cudaError = cudaGetLastError();
 
-	vector3** accels = (vector3**)malloc(sizeof(vector3*) * NUMENTITIES);
-	cudaMemcpy(accels, dev_accels, sizeof(vector3*)*NUMENTITIES, cudaMemcpyDeviceToHost);
+	sumRows<<<numBlocks, blockSize>>>(dev_accels, dev_hPos, dev_hVel);
+	cudaMemcpy(hVel, dev_hVel, sizeof(vector3*)*NUMENTITIES, cudaMemcpyDeviceToHost);
+	cudaMemcpy(hPos, dev_hPos, sizeof(vector3*)*NUMENTITIES, cudaMemcpyDeviceToHost);
+	//vector3** accels = (vector3**)malloc(sizeof(vector3*) * NUMENTITIES);
+	//cudaMemcpy(accels, dev_accels, sizeof(vector3*)*NUMENTITIES, cudaMemcpyDeviceToHost);
 	if (1) {
 		printf("CUDA Error: %s\n", cudaGetErrorString(cudaError));
 	}
 	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
-	for (i=0;i<NUMENTITIES;i++){
-		vector3 accel_sum={0,0,0};
-		for (j=0;j<NUMENTITIES;j++){
-			for (k=0;k<3;k++)
-				accel_sum[k]+=accels[i][j][k];
-		}
-		//compute the new velocity based on the acceleration and time interval
-		//compute the new position based on the velocity and time interval
-		for (k=0;k<3;k++){
-			hVel[i][k]+=accel_sum[k]*INTERVAL;
-			hPos[i][k]+=hVel[i][k]*INTERVAL;
-		}
-	}
+	
 	free(accels);
 	cudaFree(dev_hPos);
 	cudaFree(dev_mass);
