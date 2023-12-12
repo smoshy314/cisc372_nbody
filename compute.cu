ef8785246347ec31054e97a653a7e830ad8dbb2d
Josh <joshuatylermartinez@gmail.com>
2023-12-11 21:53:47 -0500

    shoudlnt have done that

--- compute.cu
+++ compute.cu
@@ -17,10 +17,11 @@ __global__ void accelComputeKernal(vector3* dev_accels, double * dev_mass, vecto
 		}else{
 			vector3 distance;
 			distance[k]= dev_hPos[i][k] - dev_hPos[j][k];
-			double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
-			double magnitude=sqrt(magnitude_sq);
-			double accelmag=-1*GRAV_CONSTANT*dev_mass[j]/magnitude_sq;
-			FILL_VECTOR(dev_accels[index],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);	
+			if(k==0){
+				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
+				double magnitude=sqrt(magnitude_sq);
+				double accelmag=-1*GRAV_CONSTANT*dev_mass[j]/magnitude_sq;
+				FILL_VECTOR(dev_accels[index],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);}	
 		}
 	}
 }
