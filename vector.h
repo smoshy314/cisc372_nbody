608f6b9ae92c2606817178890f5153e860ee3a6c
Josh <joshuatylermartinez@gmail.com>
2023-12-11 23:33:40 -0500

    externs

--- vector.h
+++ vector.h
@@ -3,8 +3,9 @@
 
 typedef double vector3[3];
 #define FILL_VECTOR(vector,a,b,c) {vector[0]=a;vector[1]=b;vector[2]=c;}
-extern vector3 *hVel, *d_hVel;
-extern vector3 *hPos, *d_hPos;
+extern vector3 *hVel, *dev_hVel;
+extern vector3 *hPos, *dev_hPos;
 extern double *mass;
 
+extern vector3* dev_accels;
 #endif
